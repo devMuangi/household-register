089507eb239a54b1a43b70c18c8197bf2a281ea8
devMuangi <devmuangi@gmail.com>
2021-07-22 10:52:08 +0300

    added dashboard

--- digitizing/src/components/Navbar.tsx
+++ digitizing/src/components/Navbar.tsx
@@ -7,7 +7,6 @@ import {
   Flex,
   Avatar,
   HStack,
-  // Link,
   IconButton,
   Button,
   Menu,
@@ -21,7 +20,6 @@ import {
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-// const Links = ["Dashboard", "CHUS", "CHVS", "HOUSEHOLDS"];
 const Links = [
   {to:'/dashboard',
     name:'Dashboard'},
@@ -32,29 +30,12 @@ const Links = [
     {to:'/HOUSEHOLDS',
   name:'HOUSEHOLDS'}
 ]
-
-// const NavLink = ({ children }: { children: ReactNode }) => (
-  
-//     <Link
-    
-//     // px={2}
-//     // py={1}
-//     // rounded={"md"}
-//     // _hover={{
-//     //   textDecoration: "none",
-//     //   bg: useColorModeValue("gray.200", "gray.700"),
-//     // }}
-//     href={"CHUS"}
-//     >
-//     {children}
-//   </Link>
-//   );
  
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <>
+    <div style={{position:'fixed',width:'100vw',zIndex:'10'}}>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
@@ -74,6 +55,7 @@ export default function Navbar() {
                   objectFit="contain"
                   width={70}
                   height={50}
+                  Link={'/'}
                 />
               </span>
             </Box>
@@ -83,7 +65,6 @@ export default function Navbar() {
               display={{ base: "none", md: "flex" }}
             >
               {Links.map((link) => (
-                // <NavLink key={link}>{link}</NavLink>
                 <Link key={link} href={link.to}>{link.name}</Link>
               ))}
             </HStack>
@@ -110,12 +91,12 @@ export default function Navbar() {
           <Box pb={4} display={{ md: "none" }}>
             <Stack as={"nav"} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <Link key={link}>{link}</Link>
               ))}
             </Stack>
           </Box>
         ) : null}
       </Box>
-    </>
+    </div>
   );
 }
